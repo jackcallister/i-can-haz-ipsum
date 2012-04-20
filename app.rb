fd40c7cc23ffac1a845c12d9a683ed366ba328a4
Jack Callister <jarsbe@gmail.com>
2012-04-21 00:20:51 +1200

    Changed app from a sentence generator to paragraphs

--- app.rb
+++ app.rb
@@ -3,42 +3,59 @@ require 'sinatra'
 require 'shotgun'
 
 helpers do
+  
   def generate_sentence
-    db = ["kitty", "cat", "lol", "haz"]
-		paragraph = ""
-		requested_sentences = "#{params[:post][:requested]}".to_i
-		init_requested_sentences = requested_sentences
+  	db = ["kitty", "cat", "lol", "haz"]
+		requested_paragraphs = "#{params[:post][:requested]}".to_i
+		paragraphs = []
 
-		until requested_sentences == 0
-			sentence_size = rand(40) + 20
-			init_sentence_size = sentence_size
-			sentence = ""
-			until sentence_size == 0
-				pull = db[rand(db.size)] 
-				
-				if sentence_size == init_sentence_size && requested_sentences == init_requested_sentences 
-					sentence << pull.capitalize
-				elsif sentence_size == init_sentence_size
-					sentence << " " + pull.capitalize
-				elsif sentence_size == 1
-					sentence << " " + pull + "."
-				else
-					sentence << " " + pull 
+		until requested_paragraphs == 0 #Make paragraphs of sentences
+
+			number_of_sentences = rand(10) + 3
+			init_number_of_sentences = number_of_sentences
+			paragraph = ""
+
+			until number_of_sentences == 0 #Make sentences of words
+
+				sentence_size = rand(40) + 5
+				init_sentence_size = sentence_size
+				sentence = ""
+
+				until sentence_size == 0
+					pull = db[rand(db.size)] 
+					
+					if sentence_size == init_sentence_size && number_of_sentences == init_number_of_sentences 
+						sentence << pull.capitalize
+					elsif sentence_size == init_sentence_size 
+						sentence << " " + pull.capitalize
+					elsif sentence_size == 1
+						sentence << " " + pull + "."
+					else
+						sentence << " " + pull 
+					end
+					
+					sentence_size = sentence_size - 1
 				end
-				
-				sentence_size = sentence_size - 1
-			end
-			paragraph << sentence
-			requested_sentences = requested_sentences - 1
-		end
-		@result = paragraph.to_s
+				paragraph << sentence 
+				number_of_sentences = number_of_sentences - 1
+
+			end	
+
+			paragraphs.push(paragraph)
+			requested_paragraphs = requested_paragraphs - 1
+
+		end  
+		@paragraphs = paragraphs
   end
+
 end
 
 get '/' do
 	erb :form
+	
 end
 
 post '/' do
 	generate_sentence
+	@paragraphs.each{|n| p n}
 end
